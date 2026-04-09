86362ff74a1f0b62700bb9ae96c14c25c365ea4a
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2026-04-09 17:57:13 -0400

    passiveagents 1.0.0-beta.8

--- Formula/passiveagents.rb
+++ Formula/passiveagents.rb
@@ -1,26 +1,26 @@
 class Passiveagents < Formula
   desc "PassiveAgents local agent manager"
   homepage "https://passiveagents.com"
-  version "1.0.0-beta.7"
+  version "1.0.0-beta.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.7/passiveagents_Darwin_arm64.tar.gz"
-      sha256 "fc20c4ec6ad46aa5c246a396f7cadfa3fe740242d64b1f1be4d03d03af79091c"
+      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.8/passiveagents_Darwin_arm64.tar.gz"
+      sha256 "641849aeaad75847cfe6dd1e612b0c227f8a133469283ad60a63accc0a923e3a"
     else
-      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.7/passiveagents_Darwin_amd64.tar.gz"
-      sha256 "fabf7a704d309752048182707eeae6707bb6f7b8ec0ebf85d6fe9716be18e827"
+      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.8/passiveagents_Darwin_amd64.tar.gz"
+      sha256 "642318a221518af3b956abb93dee490ad070df7ff3b9ec792fdb4bb8576dc14f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.7/passiveagents_Linux_arm64.tar.gz"
-      sha256 "80575fec5ae6fb489773aa4f1379b2050ce489a1356427b6dac57944ab0e856a"
+      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.8/passiveagents_Linux_arm64.tar.gz"
+      sha256 "d59bd61d70d3bf08dc3573b510b0c6cc78930a9e47bfe552b6cc868df9b326c0"
     else
-      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.7/passiveagents_Linux_amd64.tar.gz"
-      sha256 "a680e8797deaf3cc0860895a265c5b2ebe21a4d9fc8b8a1d704913ec4189ed08"
+      url "https://github.com/karthikp32/homebrew-passiveagents/releases/download/manager-v1.0.0-beta.8/passiveagents_Linux_amd64.tar.gz"
+      sha256 "01a7bc2dc31e7a5b97b6f9160a9f99f37345bb91a0844b068399eeae03bf44dc"
     end
   end
 
